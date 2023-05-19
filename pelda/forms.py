180107f6881b6f118fa45f8610ccfe8adb258bc4
lajtosrob <lajtosrobert@freemail.hu>
2023-05-19 13:24:01 +0200

    Django Video 21 - ModelForm (model űrlap) 2. rész

--- pelda/forms.py
+++ pelda/forms.py
@@ -5,4 +5,20 @@ class TermekForm(forms.ModelForm):
     class Meta:
         model = Termek
         fields = '__all__'
+
+        widgets = {
+            'azonosito': forms.TextInput(attrs={'class': 'form-control'}),
+            'nev': forms.TextInput(attrs={'class': 'form-control'}),
+            'leiras': forms.Textarea(attrs={'class': 'form-control'}),
+            'ar': forms.NumberInput(attrs={'class': 'form-control'}),
+            'raktaron': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
+
+        labels = {
+            "azonosito": "Azonosító:",
+            "nev": "Név:",
+            "leiras": "Leírás:",
+            "ar": "Ár:",
+            "raktaron": "Raktáron:"
+        }
         
